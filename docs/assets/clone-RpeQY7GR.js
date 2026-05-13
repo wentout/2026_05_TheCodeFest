79b9e61b98110628d0e57b9a5658159a82a15036
wentout <went.out@gmail.com>
2026-05-13 17:10:08 +0300

    latest changes

--- docs/assets/clone-RpeQY7GR.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./graph-D_l0kABE.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- slider/src/keyboard.ts
+++ slider/src/keyboard.ts
@@ -36,4 +36,41 @@ export default function () {
 	// 	app.clickNext();
 	// };
 
+	// Touch / swipe support for mobile
+	let touchStartX = 0;
+	let touchStartY = 0;
+	const swipeThreshold = 50;
+
+	window.document.addEventListener('touchstart', (e) => {
+		touchStartX = e.changedTouches[ 0 ].screenX;
+		touchStartY = e.changedTouches[ 0 ].screenY;
+	}, { passive : true });
+
+	window.document.addEventListener('touchend', (e) => {
+		const touchEndX = e.changedTouches[ 0 ].screenX;
+		const touchEndY = e.changedTouches[ 0 ].screenY;
+		const dx = touchEndX - touchStartX;
+		const dy = touchEndY - touchStartY;
+
+		// Horizontal swipe
+		if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > swipeThreshold) {
+			if (dx < 0) {
+				w(() => app.slideNext())();
+			} else {
+				w(() => app.slidePrev())();
+			}
+		}
+	}, { passive : true });
+
+	// Tap on screen halves
+	window.document.addEventListener('click', (e) => {
+		const width = window.innerWidth;
+		const x = e.clientX;
+		if (x > width * 0.7) {
+			w(() => app.slideNext())();
+		} else if (x < width * 0.3) {
+			w(() => app.slidePrev())();
+		}
+	});
+
 }
